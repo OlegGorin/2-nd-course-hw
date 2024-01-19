f5fd6fc7bc8345cdbcc9489198ae0ddf28fd203e
OlegGorin <olgorin@rambler.ru>
2024-01-19 14:44:38 +0200

    Correcting errors

--- hw-03.js
+++ hw-03.js
@@ -29,40 +29,24 @@ alert(Number(a) + Number(b));
 let monthNumber = Number(prompt('Введите номер месяца'));
 switch (monthNumber) {
     case 1:
-        console.log('Зима');
-        break;
     case 2:
+    case 12:
         console.log('Зима');
         break;
     case 3:
-        console.log('Весна');
-        break;
     case 4:
-        console.log('Весна');
-        break;
     case 5:
         console.log('Весна');
         break;
     case 6:
-        console.log('Лето');
-        break;
     case 7:
-        console.log('Лето');
-        break;
     case 8:
         console.log('Лето');
         break;
     case 9:
-        console.log('Осень');
-        break;
     case 10:
-        console.log('Осень');
-        break;
     case 11:
         console.log('Осень');
-        break;
-    case 12:
-        console.log('Зима');
         break;
     default:
         console.log('Введите номер месяца в диапазоне от 1 до 12');
